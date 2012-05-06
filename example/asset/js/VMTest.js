f1886f4709bde0b8f8d9263adac4426c3d333036
Matías MI <lumen.night@gmail.com>
2012-05-05 22:32:34 -0300

    Added support to compile PHP source files.
    * Moved common workspace behaviour to AbstractWorkspace class.
    * Modified FileBuffer to avoid useless inheritance.
    * Removed unnecessary JavaFileBuffer.
    * Added support to register multiple workspace handlers.
    * Added PHP adapter to check syntax on PHP source files.
    
    TODO: clean up PHP workspace.

--- example/asset/js/VMTest.js
+++ example/asset/js/VMTest.js
@@ -1,29 +1,49 @@
 
 jQuery(function () {
-  /** Processes Java results.
-   */
-  var processResult = function (buffer, result) {
-    var error;
-    var matches = function (error) {
-      var name = buffer.name;
-      name = name.substr(name.lastIndexOf(".") + 1) + ".java";
-      return name === error.source;
-    };
+  var resolvers = {
+    java: function (buffer, error) {
+      var name = "/" + buffer.name.replace(/\./ig, "/") + ".java";
 
-    if (result.errors.length) {
-      for (var i = 0;  i < result.errors.length; i++) {
-        error = result.errors[i];
-
-        if (matches(error)) {
-          buffer.mark(error.lineNumber - 1, error.columnNumber, error.message,
-            "error");
-        }
+      if (name === error.source) {
+        return error;
       }
+
+      return null;
+    },
+    php: function (buffer, error) {
+      if (error.source.indexOf(buffer.name) === 0) {
+        return error;
+      }
+
+      return null;
     }
   };
 
+  /** Constructs an error handler with the specified error resolver.
+   *
+   * @param {Function} resolver Error resolver function. It takes the buffer
+   *   and the error as parameter and it must return the resolved error. Must
+   *   return null if the error cannot be resolved.
+   */
+  var errorHandler = function (resolver) {
+    return function (buffer, result) {
+      var error;
+
+      if (result.errors.length) {
+        for (var i = 0;  i < result.errors.length; i++) {
+          error = resolver(buffer, result.errors[i]);
+
+          if (error) {
+            buffer.mark(error.lineNumber - 1, error.columnNumber,
+              error.message, "error");
+          }
+        }
+      }
+    };
+  };
+  var lang = VM.lang.PHP;
   var editor = new VM.SourceEditor(jQuery("#editor"), "ws://localhost:8000",
-    VM.lang.JAVA, processResult);
+    lang, errorHandler(resolvers[lang]));
 
   editor.render();
   editor.createBuffer("com.test.app.EntryPoint").bringToFront();
